d88df660b448759b42e62625047fc9a363c3a427
xuvish <b6mf2jsdh2@privaterelay.appleid.com>
2026-09-17 08:22:27 +0530

    Load portal fixes in order

---
+++ assets/fixes-bootstrap.js
@@ -0,0 +1,6 @@
+(function(){
+  function load(src,done){const s=document.createElement('script');s.src=src;s.onload=done;s.onerror=()=>console.error('Failed to load',src);document.body.appendChild(s)}
+  load('assets/fix-point1.js?v=20260917-0900',()=>{
+    let tries=0;const wait=setInterval(()=>{tries++;if(window.openClassification||tries>80){clearInterval(wait);load('assets/fix-point4.js?v=20260917-0900',()=>load('assets/fix-point5.js?v=20260917-0900',()=>load('assets/fix-point6.js?v=20260917-0900')))}},50)
+  })
+})();
